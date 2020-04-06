4a2a6cc194864d9ad7f990ce3309b377dbf22f37
kperanovic <karlo.peranovic@gmx.at>
2020-04-06 14:39:59 +0200

    Add communication between client and server

--- daytime/src/daytime_client/main.cpp
+++ daytime/src/daytime_client/main.cpp
@@ -2,7 +2,19 @@
 #include "asio.hpp"
 
 using namespace std;
+using namespace asio::ip;
 
 int main() {
-    cout << "hello, client" << endl;
+    string port{"2222"};
+    tcp::iostream strm{"localhost", port};
+
+    if (strm) { // connected
+        
+        string data;
+        getline(strm, data);
+        cout << data << endl;
+        strm.close();
+    } else { 
+        cerr << "Could not connect to Server" << endl;
+    }
 }
--- daytime/src/daytime_server/main.cpp
+++ daytime/src/daytime_server/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include "asio.hpp"
+#include "timeutils.h"
 
 using namespace std;
+using namespace asio::ip;
 
 int main() {
-    cout << "hello, server" << endl;
+    short unsigned port{2222};
+    asio::io_context ctx;
+    tcp::endpoint ep{tcp::v4(), port};
+    tcp::acceptor acceptor{ctx, ep};
+    acceptor.listen();
+
+   while(true) {
+        asio::ip::tcp::socket sock{ctx};
+        acceptor.accept(sock);
+        asio::ip::tcp::iostream strm{move(sock)};
+
+        if(strm) {
+            try {
+                string x{"hello"};
+                strm << x;
+            } catch(...) {
+                cerr << "Could not send data without errors" << endl;
+            }
+        } else {
+            cerr << "Could not accept connection without errors" << endl;
+        }
+        
+        strm.close();
+    }
 }
